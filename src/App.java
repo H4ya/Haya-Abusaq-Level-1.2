2716779635b745d79355fe40dda87056caed25f4
Haya <145580738+H4ya@users.noreply.github.com>
2024-02-17 19:31:18 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,9 +4,11 @@ public class App {
     public static void main(String[] args){
         
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of x: ");
         double x = sc.nextDouble();
+        System.out.print("Enter the value of y: ");
         double y = sc.nextDouble();
-
+        System.out.println(/*Just an empty line*/);
         if ( x > y )
           System.out.println(x +" is bigger than " + y) ;
         else if (y > x)
@@ -15,8 +17,6 @@ public class App {
           System.out.println("Both x and y are equal");
         
 
-
-      System.out.println();
     }
 }
 
